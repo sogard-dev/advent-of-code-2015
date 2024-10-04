f9218c57be77749a3e253ef3e2c351df55698765
morten <morten.sogard.jensen@gmail.com>
2024-10-04 11:28:05 +0000

    day 3

--- day3/task.go
+++ day3/task.go
@@ -1,9 +1,56 @@
 package day3
 
+type pos struct {
+	x int
+	y int
+}
+
 func part1(input string) int {
-	return 0
+	visited := map[pos]bool{}
+	visited[pos{}] = true
+
+	santaPos := pos{}
+	for _, c := range input {
+		switch c {
+		case '>':
+			santaPos = pos{santaPos.x + 1, santaPos.y}
+		case '<':
+			santaPos = pos{santaPos.x - 1, santaPos.y}
+		case '^':
+			santaPos = pos{santaPos.x, santaPos.y - 1}
+		case 'v':
+			santaPos = pos{santaPos.x, santaPos.y + 1}
+		}
+		visited[santaPos] = true
+	}
+
+	return len(visited)
 }
 
 func part2(input string) int {
-	return 0
+	visited := map[pos]bool{}
+	visited[pos{}] = true
+
+	santaPos := pos{}
+	robotSantaPos := pos{}
+	for i, c := range input {
+		moving := &santaPos
+		if i%2 == 1 {
+			moving = &robotSantaPos
+		}
+
+		switch c {
+		case '>':
+			moving.x += 1
+		case '<':
+			moving.x -= 1
+		case '^':
+			moving.y -= 1
+		case 'v':
+			moving.y += 1
+		}
+		visited[*moving] = true
+	}
+
+	return len(visited)
 }
--- day3/task_test.go
+++ day3/task_test.go
@@ -8,9 +8,15 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 2, part1(">"))
+	require.Equal(t, 4, part1("^>v<"))
+	require.Equal(t, 2, part1("^v^v^v^v^v"))
+	require.Equal(t, 2081, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 3, part2("^v"))
+	require.Equal(t, 3, part2("^>v<"))
+	require.Equal(t, 11, part2("^v^v^v^v^v"))
+	require.Equal(t, 2341, part2(utils.GetInput(t, "input.txt")))
 }
