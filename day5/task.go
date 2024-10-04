f592656b79bde63c7cc832214941728b47faa5bf
morten <morten.sogard.jensen@gmail.com>
2024-10-04 12:42:07 +0000

    day 5

--- day5/task.go
+++ day5/task.go
@@ -1,9 +1,78 @@
 package day5
 
+import "strings"
+
 func part1(input string) int {
-	return 0
+	var result int
+	for _, word := range strings.Split(input, "\n") {
+		if isNicePart1(word) {
+			result += 1
+		}
+	}
+	return result
+}
+
+func isNicePart1(word string) bool {
+	badArr := [...]string{"ab", "cd", "pq", "xy"}
+	for _, bad := range badArr {
+		if strings.Contains(word, bad) {
+			return false
+		}
+	}
+
+	vowelCount := 0
+	previousRune := ' '
+	letterTwice := false
+
+	for _, r := range word {
+		switch r {
+		case 'a', 'e', 'i', 'o', 'u':
+			vowelCount += 1
+		}
+
+		if r == previousRune {
+			letterTwice = true
+		}
+
+		previousRune = r
+	}
+	return vowelCount >= 3 && letterTwice
 }
 
 func part2(input string) int {
-	return 0
+	var result int
+	for _, word := range strings.Split(input, "\n") {
+		if isNicePart2(word) {
+			result += 1
+		}
+	}
+	return result
+}
+
+func isNicePart2(word string) bool {
+	twoLetters := map[string]int{}
+	hasTwo := false
+	for i := range len(word) - 1 {
+		two := string(word[i : i+2])
+		prev, ok := twoLetters[two]
+		if ok {
+			if prev < i-1 {
+				hasTwo = true
+				break
+			}
+		} else {
+			twoLetters[two] = i
+		}
+	}
+
+	hasRepeats := false
+	for i := range len(word) - 2 {
+		letter := word[i]
+		if letter == word[i+2] {
+			hasRepeats = true
+			break
+		}
+	}
+
+	return hasTwo && hasRepeats
 }
--- day5/task_test.go
+++ day5/task_test.go
@@ -8,11 +8,19 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(""))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 1, part1("ugknbfddgicrmopn"))
+	require.Equal(t, 1, part1("aaa"))
+	require.Equal(t, 0, part1("jchzalrnumimnmhp"))
+	require.Equal(t, 0, part1("haegwjzuvuyypxyu"))
+	require.Equal(t, 0, part1("dvszwmarrgswjxmb"))
+	require.Equal(t, 238, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(""))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 0, part2("aaa"))
+	require.Equal(t, 1, part2("qjhvhtzxzqqjkmpb"))
+	require.Equal(t, 1, part2("xxyxx"))
+	require.Equal(t, 0, part2("uurcxstgmygtbstg"))
+	require.Equal(t, 0, part2("ieodomkazucvgmuy"))
+	require.Equal(t, 69, part2(utils.GetInput(t, "input.txt")))
 }
