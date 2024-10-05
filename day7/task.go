a40758a5cb9225632dd43e5a93dd898fd47b5eb9
morten <morten.sogard.jensen@gmail.com>
2024-10-05 15:43:38 +0000

    day 7

--- day7/task.go
+++ day7/task.go
@@ -1,9 +1,151 @@
 package day7
 
-func part1(input string) int {
-	return 0
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type node interface {
+	value(map[string]node, map[string]uint16) uint16
+}
+
+type signal struct{ val string }
+type not struct{ n string }
+type and struct{ left, right string }
+type or struct{ left, right string }
+type rshift struct {
+	n  string
+	by uint16
+}
+type lshift struct {
+	n  string
+	by uint16
+}
+
+func valueOfNode(nodes map[string]node, memory map[string]uint16, n string) uint16 {
+	mem, ok := memory[n]
+	if ok {
+		return mem
+	}
+
+	node, ok := nodes[n]
+	if !ok {
+		num, ok := getNum(n)
+		if !ok {
+			panic("Could not find node and is not number" + n)
+		}
+		memory[n] = num
+		return num
+	}
+	ret := node.value(nodes, memory)
+	memory[n] = ret
+	return ret
+}
+
+func (n signal) value(nodes map[string]node, memory map[string]uint16) uint16 {
+	return valueOfNode(nodes, memory, n.val)
+}
+
+func (n and) value(nodes map[string]node, memory map[string]uint16) uint16 {
+	return valueOfNode(nodes, memory, n.left) & valueOfNode(nodes, memory, n.right)
+}
+
+func (n or) value(nodes map[string]node, memory map[string]uint16) uint16 {
+	return valueOfNode(nodes, memory, n.left) | valueOfNode(nodes, memory, n.right)
+}
+
+func (n not) value(nodes map[string]node, memory map[string]uint16) uint16 {
+	return ^valueOfNode(nodes, memory, n.n)
+}
+
+func (n rshift) value(nodes map[string]node, memory map[string]uint16) uint16 {
+	return valueOfNode(nodes, memory, n.n) >> n.by
+}
+
+func (n lshift) value(nodes map[string]node, memory map[string]uint16) uint16 {
+	return valueOfNode(nodes, memory, n.n) << n.by
+}
+
+func getNum(str string) (uint16, bool) {
+	v, err := strconv.ParseInt(str, 10, 16)
+	if err != nil {
+		return 0, false
+	}
+	return uint16(v), true
+}
+
+func part1(input string, endWire string) int {
+	nodes := map[string]node{}
+	memory := map[string]uint16{}
+
+	for _, line := range strings.Split(input, "\n") {
+		node, outputWire := createNode(line)
+		nodes[outputWire] = node
+	}
+
+	return int(nodes[endWire].value(nodes, memory))
+}
+
+func createNode(line string) (node, string) {
+	sides := strings.Split(line, " -> ")
+	lside := sides[0]
+	outputWire := sides[1]
+
+	lspl := strings.Split(lside, " ")
+
+	if len(lspl) == 1 {
+		// 123 -> x
+		return signal{lspl[0]}, outputWire
+	} else if len(lspl) == 2 {
+		// NOT lk -> ll
+		operator := lspl[0]
+		wire := lspl[1]
+		if operator == "NOT" {
+			return not{
+				n: wire,
+			}, outputWire
+		}
+	} else {
+		operator := lspl[1]
+		arg1 := lspl[0]
+		arg2 := lspl[2]
+
+		// gk OR gq -> gr
+		// ff AND fh -> fi
+		// kf LSHIFT 15 -> kj
+
+		switch operator {
+		case "OR":
+			return or{
+				left:  arg1,
+				right: arg2,
+			}, outputWire
+		case "AND":
+			return and{
+				left:  arg1,
+				right: arg2,
+			}, outputWire
+		case "LSHIFT":
+			num, _ := getNum(arg2)
+			return lshift{
+				n:  arg1,
+				by: num,
+			}, outputWire
+		case "RSHIFT":
+			num, _ := getNum(arg2)
+			return rshift{
+				n:  arg1,
+				by: num,
+			}, outputWire
+		}
+	}
+	panic("Unknown operator " + line)
 }
 
 func part2(input string) int {
-	return 0
+	wireA := part1(input, "a")
+	newInput := strings.ReplaceAll(input, "14146 -> b", fmt.Sprintf("%d -> b", wireA))
+	newWireA := part1(newInput, "a")
+	return int(newWireA)
 }
--- day7/task_test.go
+++ day7/task_test.go
@@ -8,11 +8,26 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(""))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	testString := `123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i`
+	require.Equal(t, 72, part1(testString, "d"))
+	require.Equal(t, 507, part1(testString, "e"))
+	require.Equal(t, 492, part1(testString, "f"))
+	require.Equal(t, 114, part1(testString, "g"))
+	require.Equal(t, 65412, part1(testString, "h"))
+	require.Equal(t, 65079, part1(testString, "i"))
+	require.Equal(t, 123, part1(testString, "x"))
+	require.Equal(t, 456, part1(testString, "y"))
+
+	require.Equal(t, 956, part1(utils.GetInput(t, "input.txt"), "a"))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(""))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 40149, part2(utils.GetInput(t, "input.txt")))
 }
