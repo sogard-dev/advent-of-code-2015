c335de223c162a9f4c6ef2968bdd2cc8694af875
morten <morten.sogard.jensen@gmail.com>
2024-10-06 05:19:10 +0000

    day 8

--- day8/task.go
+++ day8/task.go
@@ -1,9 +1,66 @@
 package day8
 
+import "strings"
+
 func part1(input string) int {
-	return 0
+	var charsInString int
+	var charsInMemory int
+	for _, line := range strings.Split(input, "\n") {
+		charsInString += len(line)
+
+		i := 1
+		length := 0
+		var prev byte
+		for i < len(line)-1 {
+			thisChar := line[i]
+			toSetPrev := thisChar
+
+			if prev == '\\' {
+				if thisChar == '"' {
+					toSetPrev = 0
+				} else if thisChar == '\\' {
+					toSetPrev = 0
+				} else if thisChar == 'x' {
+					toSetPrev = 0
+					i += 2
+				} else {
+					panic("Unknown escape")
+				}
+			} else {
+				length += 1
+			}
+
+			prev = toSetPrev
+			i++
+		}
+
+		charsInMemory += length
+	}
+	return charsInString - charsInMemory
 }
 
 func part2(input string) int {
-	return 0
+	var charsInString int
+	var charsEscaped int
+	for _, line := range strings.Split(input, "\n") {
+		charsInString += len(line)
+
+		i := 0
+		length := 0
+		for i < len(line) {
+			thisChar := line[i]
+
+			switch thisChar {
+			case '"', '\\':
+				length += 2
+			default:
+				length += 1
+			}
+
+			i++
+		}
+
+		charsEscaped += length + 2
+	}
+	return charsEscaped - charsInString
 }
--- day8/task_test.go
+++ day8/task_test.go
@@ -8,11 +8,29 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(""))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 3, part1(`"\\x27"`))
+	require.Equal(t, 6, part1(`"\\\x27"`))
+	require.Equal(t, 5, part1(`"\x27"`))
+	require.Equal(t, 3, part1(`"aaa\"aaa"`))
+	require.Equal(t, 2, part1(`"abc"`))
+	require.Equal(t, 4, part1(`"\\\\"`))
+	require.Equal(t, 2, part1(`"j"`))
+	require.Equal(t, 4, part1(`"\\\\zkisyjpbzandqikqjqvee"`))
+	require.Equal(t, 12, part1(`""
+"abc"
+"aaa\"aaa"
+"\x27"`))
+	require.Equal(t, 1333, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(""))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 4, part2(`""`))
+	require.Equal(t, 4, part2(`"abc"`))
+	require.Equal(t, 6, part2(`"aaa\"aaa"`))
+	require.Equal(t, 5, part2(`"\x27"`))
+	require.Equal(t, 19, part2(`""
+"abc"
+"aaa\"aaa"
+"\x27"`))
+	require.Equal(t, 2046, part2(utils.GetInput(t, "input.txt")))
 }
