4c9de78062b99a7dd1806064566e2d10ca9fcaf1
morten <morten.sogard.jensen@gmail.com>
2024-10-06 11:51:20 +0000

    day 10

--- day10/task.go
+++ day10/task.go
@@ -1,9 +1,74 @@
 package day10
 
-func part1(input string) int {
-	return 0
+import "strconv"
+
+type block struct {
+	number    int
+	repeating int
+	nextBlock *block
 }
 
-func part2(input string) int {
-	return 0
+func (b *block) truncate() {
+	for {
+		if b.nextBlock == nil || b.number != b.nextBlock.number {
+			break
+		} else {
+			b.repeating += b.nextBlock.repeating
+			b.nextBlock = b.nextBlock.nextBlock
+		}
+	}
+
+	if b.nextBlock != nil {
+		b.nextBlock.truncate()
+	}
+}
+
+func (b *block) iterate() {
+	if b.repeating == 0 {
+		b.nextBlock.iterate()
+	} else {
+		newBlock := &block{
+			number:    b.number,
+			repeating: 1,
+			nextBlock: b.nextBlock,
+		}
+		b.number = b.repeating
+		b.nextBlock = newBlock
+		b.repeating = 1
+
+		if newBlock.nextBlock != nil {
+			newBlock.nextBlock.iterate()
+		}
+	}
+}
+
+func (b *block) count() int {
+	if b.nextBlock == nil {
+		return b.repeating
+	} else {
+		return b.repeating + b.nextBlock.count()
+	}
+}
+
+func part1(input string, iterations int) int {
+	rootBlock := block{}
+	lastBlock := &rootBlock
+	for _, s := range input {
+		num, _ := strconv.Atoi(string(s))
+		newBlock := &block{
+			number:    num,
+			repeating: 1,
+		}
+
+		lastBlock.nextBlock = newBlock
+		lastBlock = newBlock
+	}
+
+	rootBlock.truncate()
+	for range iterations {
+		rootBlock.iterate()
+		rootBlock.truncate()
+	}
+
+	return rootBlock.count()
 }
--- day10/task_test.go
+++ day10/task_test.go
@@ -8,11 +8,14 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(""))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 2, part1(`1`, 1))
+	require.Equal(t, 2, part1(`11`, 1))
+	require.Equal(t, 4, part1(`21`, 1))
+	require.Equal(t, 6, part1(`1211`, 1))
+	require.Equal(t, 6, part1(`111221`, 1))
+	require.Equal(t, 492982, part1(utils.GetInput(t, "input.txt"), 40))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(""))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 6989950, part1(utils.GetInput(t, "input.txt"), 50))
 }
