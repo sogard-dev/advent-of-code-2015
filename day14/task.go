fa1648e4904c8cdd87941fd0c364b46406352620
morten <morten.sogard.jensen@gmail.com>
2024-10-08 11:36:53 +0000

    day 14

--- day14/task.go
+++ day14/task.go
@@ -1,9 +1,74 @@
 package day14
 
-func part1(input string) int {
-	return 0
+import (
+	"slices"
+	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2015/utils"
+)
+
+func part1(input string, rounds int) int {
+	deers := parse(input)
+
+	longest := 0
+	for _, deer := range deers {
+		longest = max(deer.atSecond(rounds), longest)
+	}
+
+	return longest
 }
 
-func part2(input string) int {
-	return 0
+func part2(input string, rounds int) int {
+	deers := parse(input)
+	points := make([]int, len(deers))
+	for sec := range rounds {
+		longest := 1
+		longestIndexes := []int{}
+		for i, deer := range deers {
+			distance := deer.atSecond(sec)
+			if distance > longest {
+				longest = distance
+				longestIndexes = []int{i}
+			} else if distance == longest {
+				longestIndexes = append(longestIndexes, i)
+			}
+		}
+
+		for _, k := range longestIndexes {
+			points[k]++
+		}
+	}
+
+	return slices.Max(points)
+}
+
+type deer struct {
+	speed    int
+	duration int
+	rest     int
+}
+
+func (d deer) atSecond(sec int) int {
+	cycle := d.duration + d.rest
+	distancePerCycle := d.duration * d.speed
+	cycles := sec / cycle
+	timeIntoCycle := min(sec%cycle, d.duration)
+	return cycles*distancePerCycle + timeIntoCycle*d.speed
+
+}
+
+func parse(input string) []deer {
+	ret := []deer{}
+	for _, line := range strings.Split(input, "\n") {
+		numbers := utils.GetAllNumbers(line)
+		speed, duration, rest := numbers[0], numbers[1], numbers[2]
+
+		ret = append(ret, deer{
+			speed:    speed,
+			duration: duration,
+			rest:     rest,
+		})
+
+	}
+	return ret
 }
--- day14/task_test.go
+++ day14/task_test.go
@@ -8,11 +8,13 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 1120, part1(`Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`, 1000))
+	require.Equal(t, 2696, part1(utils.GetInput(t, "input.txt"), 2503))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 689, part2(`Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.`, 1000))
+	require.Equal(t, 1084, part2(utils.GetInput(t, "input.txt"), 2503))
 }
