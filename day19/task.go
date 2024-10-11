90d296ca45645ddb43008a28aa906503127e3447
morten <morten.sogard.jensen@gmail.com>
2024-10-11 07:28:02 +0000

    day 19

--- day19/task.go
+++ day19/task.go
@@ -1,20 +1,101 @@
 package day19
 
 import (
-	"fmt"
+	"math"
+	"strconv"
 	"strings"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+type rule struct {
+	in  string
+	out string
 }
 
-func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+func parse(rulesStr string) []rule {
+	rules := make([]rule, 0)
+	for _, line := range strings.Split(rulesStr, "\n") {
+		spl := strings.Split(line, " => ")
+		rules = append(rules, rule{
+			in:  spl[0],
+			out: spl[1],
+		})
 	}
-	return 0
+	return rules
+}
+
+func part1(rulesStr, molecule string) int {
+	rules := parse(rulesStr)
+	return options(molecule, rules)
+}
+
+func options(molecule string, rules []rule) int {
+	seenMolecules := map[string]bool{}
+	for _, r := range rules {
+		for _, s := range getManipulatedMolecules(molecule, r, false) {
+			seenMolecules[s] = true
+		}
+	}
+	return len(seenMolecules)
+}
+
+func getManipulatedMolecules(molecule string, r rule, part2 bool) []string {
+	seenMolecules := map[string]bool{}
+	toReplace := r.in
+	toReplaceWith := r.out
+	if part2 {
+		toReplace = r.out
+		toReplaceWith = r.in
+	}
+	for i := 0; i <= len(molecule)-len(toReplace); i++ {
+		matches := true
+		for k := range len(toReplace) {
+			matches = matches && toReplace[k] == molecule[i+k]
+		}
+		if matches {
+			pref := molecule[0:i]
+			suff := molecule[i+len(toReplace):]
+			newStr := pref + toReplaceWith + suff
+			seenMolecules[newStr] = true
+		}
+	}
+	options := []string{}
+	for k := range seenMolecules {
+		options = append(options, k)
+	}
+	return options
+}
+
+func part2(rulesStr, molecule string) int {
+	rules := parse(rulesStr)
+
+	return recurse(rules, molecule)
+}
+
+func recurse(rules []rule, molecule string) int {
+	ret := math.MaxInt
+	seenMolecules := map[string]bool{}
+
+	var inner func(m string, manipulations int)
+	inner = func(m string, manipulations int) {
+		if m == "e" {
+			ret = min(ret, manipulations)
+			println("Found " + strconv.Itoa(manipulations))
+			return
+		}
+		_, ok := seenMolecules[m]
+		if m == "" || manipulations >= ret || ok {
+			return
+		}
+		seenMolecules[m] = true
+
+		for _, r := range rules {
+			for _, s := range getManipulatedMolecules(m, r, true) {
+				inner(s, manipulations+1)
+			}
+		}
+	}
+
+	inner(molecule, 0)
+
+	return ret
 }
--- day19/task_test.go
+++ day19/task_test.go
@@ -8,11 +8,21 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	testRules := `H => HO
+H => OH
+O => HH`
+	require.Equal(t, 4, part1(testRules, `HOH`))
+	require.Equal(t, 7, part1(testRules, `HOHOHO`))
+	require.Equal(t, 518, part1(utils.GetInput(t, "input.txt"), `CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl`))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	testRules := `e => H
+e => O
+H => HO
+H => OH
+O => HH`
+	require.Equal(t, 3, part2(testRules, "HOH"))
+	require.Equal(t, 6, part2(testRules, "HOHOHO"))
+	require.Equal(t, 200, part2(utils.GetInput(t, "input.txt"), `CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl`))
 }
