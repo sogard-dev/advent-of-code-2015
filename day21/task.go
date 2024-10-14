e659107b74a312058830424765a348af426c36ff
morten <morten.sogard.jensen@gmail.com>
2024-10-14 17:45:55 +0000

    day 21

--- day21/task.go
+++ day21/task.go
@@ -1,20 +1,119 @@
 package day21
 
 import (
-	"fmt"
+	"math"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2015/utils"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+type unit struct {
+	armor     int
+	hitpoints int
+	damage    int
 }
 
-func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+type item struct {
+	armor  int
+	damage int
+	cost   int
+}
+
+func parse(input string, optional bool) []item {
+	ret := []item{}
+	if optional {
+		ret = append(ret, item{})
 	}
-	return 0
+	for _, line := range strings.Split(input, "\n") {
+		nums := utils.GetAllNumbers(line)
+		ret = append(ret, item{
+			cost:   nums[0],
+			damage: nums[1],
+			armor:  nums[2],
+		})
+	}
+	return ret
+}
+
+func part1(player unit, boss unit) int {
+	lowestCost := math.MaxInt
+
+	solve(player, boss, func(cost int, won bool) {
+		if won && cost < lowestCost {
+			lowestCost = cost
+		}
+	})
+
+	return lowestCost
+}
+
+func part2(player unit, boss unit) int {
+	highestCost := 0
+
+	solve(player, boss, func(cost int, won bool) {
+		if !won && highestCost < cost {
+			highestCost = cost
+		}
+	})
+
+	return highestCost
+}
+
+func solve(player unit, boss unit, callback func(int, bool)) {
+	weapons := parse(`Dagger        8     4       0
+Shortsword   10     5       0
+Warhammer    25     6       0
+Longsword    40     7       0
+Greataxe     74     8       0`, false)
+
+	chests := parse(`Leather      13     0       1
+Chainmail    31     0       2
+Splintmail   53     0       3
+Bandedmail   75     0       4
+Platemail   102     0       5`, true)
+
+	rings := parse(`Damage     25     1       0
+Damage     50     2       0
+Damage    100     3       0
+Defense   20     0       1
+Defense   40     0       2
+Defense   80     0       3`, true)
+
+	for _, weapon := range weapons {
+		for _, chest := range chests {
+			for i, ring1 := range rings {
+				for _, ring2 := range rings {
+					if ring1 != ring2 || i == 0 {
+						cost := weapon.cost + chest.cost + ring1.cost + ring2.cost
+						armor := weapon.armor + chest.armor + ring1.armor + ring2.armor
+						damage := weapon.damage + chest.damage + ring1.damage + ring2.damage
+
+						newPlayer := unit{
+							hitpoints: player.hitpoints,
+							armor:     player.armor + armor,
+							damage:    player.damage + damage,
+						}
+
+						result := fight(newPlayer, boss)
+
+						callback(cost, result)
+					}
+				}
+			}
+		}
+	}
+}
+
+func fight(player unit, boss unit) bool {
+	for {
+		boss.hitpoints -= max(1, player.damage-boss.armor)
+		if boss.hitpoints <= 0 {
+			return true
+		}
+
+		player.hitpoints -= max(1, boss.damage-player.armor)
+		if player.hitpoints <= 0 {
+			return false
+		}
+	}
 }
--- day21/task_test.go
+++ day21/task_test.go
@@ -3,16 +3,33 @@ package day21
 import (
 	"testing"
 
-	"github.com/sogard-dev/advent-of-code-2015/utils"
 	"github.com/stretchr/testify/require"
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 78, part1(
+		unit{
+			hitpoints: 100,
+			armor:     0,
+			damage:    0,
+		},
+		unit{
+			hitpoints: 104,
+			damage:    8,
+			armor:     1,
+		}))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 148, part2(
+		unit{
+			hitpoints: 100,
+			armor:     0,
+			damage:    0,
+		},
+		unit{
+			hitpoints: 104,
+			damage:    8,
+			armor:     1,
+		}))
 }
