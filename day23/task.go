33daffc864e8598c2f7ace8ee005dc9ebce23f34
morten <morten.sogard.jensen@gmail.com>
2024-10-15 18:15:45 +0000

    day 23

--- day23/task.go
+++ day23/task.go
@@ -1,20 +1,130 @@
 package day23
 
 import (
-	"fmt"
 	"strings"
+
+	"github.com/sogard-dev/advent-of-code-2015/utils"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+type machine struct {
+	ins       int
+	registers []int
+}
+
+type instruction interface {
+	execute(*machine)
+}
+
+type hlf struct {
+	register int
+}
+
+type tpl struct {
+	register int
+}
+
+type inc struct {
+	register int
+}
+
+type jmp struct {
+	offset int
+}
+
+type jie struct {
+	register int
+	offset   int
+}
+
+type jio struct {
+	register int
+	offset   int
+}
+
+func (s hlf) execute(m *machine) {
+	m.registers[s.register] /= 2
+	m.ins++
+}
+
+func (s tpl) execute(m *machine) {
+	m.registers[s.register] *= 3
+	m.ins++
+}
+
+func (s inc) execute(m *machine) {
+	m.registers[s.register] += 1
+	m.ins++
+}
+
+func (s jmp) execute(m *machine) {
+	m.ins += s.offset
+}
+
+func (s jie) execute(m *machine) {
+	if m.registers[s.register]%2 == 0 {
+		m.ins += s.offset
+	} else {
+		m.ins++
 	}
-	return 0
+}
+
+func (s jio) execute(m *machine) {
+	if m.registers[s.register] == 1 {
+		m.ins += s.offset
+	} else {
+		m.ins++
+	}
+}
+
+func registerToInt(inp byte) int {
+	return int(inp) - 'a'
+}
+
+func solve(input string, registers []int) {
+	m := machine{
+		registers: registers,
+	}
+
+	instructions := []instruction{}
+	for _, line := range strings.Split(input, "\n") {
+		args := strings.Split(strings.ReplaceAll(line, ",", ""), " ")
+		switch args[0] {
+		case "inc":
+			instructions = append(instructions, inc{register: registerToInt(args[1][0])})
+		case "hlf":
+			instructions = append(instructions, hlf{register: registerToInt(args[1][0])})
+		case "tpl":
+			instructions = append(instructions, tpl{register: registerToInt(args[1][0])})
+		case "jmp":
+			instructions = append(instructions, jmp{offset: utils.GetAllNumbers(line)[0]})
+		case "jie":
+			instructions = append(instructions, jie{
+				offset:   utils.GetAllNumbers(line)[0],
+				register: registerToInt(args[1][0]),
+			})
+		case "jio":
+			instructions = append(instructions, jio{
+				offset:   utils.GetAllNumbers(line)[0],
+				register: registerToInt(args[1][0]),
+			})
+		default:
+			panic("Could not handle " + args[0])
+		}
+	}
+
+	for m.ins < len(instructions) {
+		instructions[m.ins].execute(&m)
+	}
+}
+
+func part1(input string) int {
+	r := []int{0, 0}
+	solve(input, r)
+	return r[1]
 }
 
 func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+	r := []int{1, 0}
+	solve(input, r)
+	return r[1]
 }
--- day23/task_test.go
+++ day23/task_test.go
@@ -8,11 +8,13 @@ import (
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 2, part1(`inc b
+jio b, +2
+tpl b
+inc b`))
+	require.Equal(t, 307, part1(utils.GetInput(t, "input.txt")))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 160, part2(utils.GetInput(t, "input.txt")))
 }
