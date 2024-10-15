7fa462cb72762d1e5166e0845076602b4a2dbd93
morten <morten.sogard.jensen@gmail.com>
2024-10-15 12:41:39 +0000

    day 22

--- day22/task.go
+++ day22/task.go
@@ -1,20 +1,162 @@
 package day22
 
 import (
-	"fmt"
-	"strings"
+	"math"
 )
 
-func part1(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
-	}
-	return 0
+type gameState struct {
+	playerHp    int
+	playerMana  int
+	bossHp      int
+	bossDamange int
+	poison      int
+	shield      int
+	recharge    int
+	playerTurn  bool
 }
 
-func part2(input string) int {
-	for _, line := range strings.Split(input, "\n") {
-		fmt.Println(line)
+type spell struct {
+	cost     int
+	damage   int
+	healing  int
+	poison   int
+	shield   int
+	recharge int
+}
+
+func (ds spell) canCast(state *gameState) bool {
+	if ds.cost >= state.playerMana {
+		return false
 	}
-	return 0
+
+	if ds.poison > 0 {
+		return state.poison == 0
+	}
+	if ds.recharge > 0 {
+		return state.recharge == 0
+	}
+	if ds.shield > 0 {
+		return state.shield == 0
+	}
+
+	return true
+}
+
+func (ds spell) action(state gameState) gameState {
+	state.playerMana -= ds.cost
+	state.playerHp += ds.healing
+	state.bossHp -= ds.damage
+	state.poison = max(state.poison, ds.poison)
+	state.recharge = max(state.recharge, ds.recharge)
+	state.shield = max(state.shield, ds.shield)
+	return state
+}
+
+func (ds spell) getCost() int {
+	return ds.cost
+}
+
+func part1(playerHitpoints, playerMana, bossHitpoints, bossDamage int) int {
+	return solve(gameState{
+		playerHp:    playerHitpoints,
+		playerMana:  playerMana,
+		bossHp:      bossHitpoints,
+		bossDamange: bossDamage,
+		playerTurn:  true,
+	}, false)
+}
+
+func part2(playerHitpoints, playerMana, bossHitpoints, bossDamage int) int {
+	return solve(gameState{
+		playerHp:    playerHitpoints,
+		playerMana:  playerMana,
+		bossHp:      bossHitpoints,
+		bossDamange: bossDamage,
+		playerTurn:  true,
+	}, true)
+}
+
+func solve(initGameSate gameState, hardmode bool) int {
+	spells := []spell{
+		{
+			cost:   173,
+			poison: 6,
+		},
+		{
+			cost:     229,
+			recharge: 5,
+		},
+		{
+			cost:   53,
+			damage: 4,
+		},
+		{
+			cost:   113,
+			shield: 6,
+		},
+		{
+			cost:    73,
+			damage:  2,
+			healing: 2,
+		},
+	}
+
+	lowest := math.MaxInt
+	playerWon := func(manacost int) {
+		lowest = min(lowest, manacost)
+	}
+
+	var simulate func(gs gameState, manaSpent int)
+	simulate = func(gs gameState, manaSpent int) {
+		if manaSpent > lowest {
+			return
+		}
+
+		isPlayerTurn := gs.playerTurn
+		gs.playerTurn = !gs.playerTurn
+
+		//handle effects
+		if isPlayerTurn && hardmode {
+			gs.playerHp -= 1
+		}
+		if gs.recharge > 0 {
+			gs.playerMana += 101
+			gs.recharge -= 1
+		}
+		if gs.shield > 0 {
+			gs.shield -= 1
+		}
+		if gs.poison > 0 {
+			gs.poison = gs.poison - 1
+			gs.bossHp -= 3
+		}
+
+		if gs.playerHp <= 0 {
+			return
+		}
+		if gs.bossHp <= 0 {
+			playerWon(manaSpent)
+			return
+		}
+
+		//handle turn
+		if isPlayerTurn {
+			for _, spell := range spells {
+				if spell.canCast(&gs) {
+					simulate(spell.action(gs), manaSpent+spell.getCost())
+				}
+			}
+		} else {
+			armor := 0
+			if gs.shield > 0 {
+				armor += 7
+			}
+			gs.playerHp -= max(1, gs.bossDamange-armor)
+			simulate(gs, manaSpent)
+		}
+	}
+
+	simulate(initGameSate, 0)
+
+	return lowest
 }
--- day22/task_test.go
+++ day22/task_test.go
@@ -3,16 +3,13 @@ package day22
 import (
 	"testing"
 
-	"github.com/sogard-dev/advent-of-code-2015/utils"
 	"github.com/stretchr/testify/require"
 )
 
 func TestPart1(t *testing.T) {
-	require.Equal(t, 0, part1(``))
-	require.Equal(t, 0, part1(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 900, part1(50, 500, 51, 9))
 }
 
 func TestPart2(t *testing.T) {
-	require.Equal(t, 0, part2(``))
-	require.Equal(t, 0, part2(utils.GetInput(t, "input.txt")))
+	require.Equal(t, 1216, part2(50, 500, 51, 9))
 }
